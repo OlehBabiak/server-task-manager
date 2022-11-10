3be4291910a897a76f4eb967c1146c0137087f2b
Oleh <oleg_bob@ukr.net>
2022-11-10 20:33:50 +0200

    some changes

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ mongoose
 		console.log("no connection ");
 	});
 
-
+const corsOptions ={
+	origin:ENV.APP.URL,
+	credentials:true,            //access-control-allow-credentials:true
+	optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('tiny'));
 
